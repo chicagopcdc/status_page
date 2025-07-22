178f3a512c96e7c4c54e6c0c559944c8836d67fe
Rachel Liu <hi.rachelliu@gmail.com>
2025-07-22 11:31:34 -0400

    Render statuses as they're fetched

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,6 +1,6 @@
 import './Spinner.css';
 
-function Spinner({ text = '', type = 'dots' }) {
+function Spinner({ text = '', type = 'dots', width = '60', height = '20' }) {
   if (type === 'spinning')
     // spinning spinner
     return (
@@ -14,12 +14,16 @@ function Spinner({ text = '', type = 'dots' }) {
   // dots
   return (
     <div className='spinner'>
-      <svg className='spinner__svg' width='60' height='20' viewBox='0 0 60 20'>
+      <svg
+        className='spinner__svg'
+        width={width}
+        height={height}
+        viewBox='0 0 60 20'>
         <circle cx='7' cy='15' r='4' />
         <circle cx='30' cy='15' r='4' />
         <circle cx='53' cy='15' r='4' />
       </svg>
-      {text && <div className='spinner__text'> {text} </div>}
+      <p> {text && <div className='spinner__text'> {text} </div>} </p>
     </div>
   );
 }
